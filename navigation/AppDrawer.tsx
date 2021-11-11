f4b32fafd2357514e117b51a7b1b23ef2ca26415
arnoldc <arnold.camas@gmail.com>
2021-11-11 08:46:24 +0800

    remove e2e2

--- navigation/AppDrawer.tsx
+++ navigation/AppDrawer.tsx
@@ -4,7 +4,6 @@ import {createDrawerNavigator} from '@react-navigation/drawer';
 import BiometricPage from '../screens/BiometricPage';
 import QRCodePage from '../screens/QRCodePage';
 import BiometricSimplepage from '../screens/BiometricSimplePage';
-import E2EPage from '../screens/E2EPage';
 
 const Drawer = createDrawerNavigator();
 
@@ -14,7 +13,6 @@ const AppDrawer = () => {
        <Drawer.Screen name="Biometrics Simple" component={BiometricSimplepage} />
         <Drawer.Screen name="Biometrics Complex" component={BiometricPage} />
         <Drawer.Screen name="QRCode" component={QRCodePage} />
-        <Drawer.Screen name="E2E" component={E2EPage} />
       </Drawer.Navigator>
     );
   };
